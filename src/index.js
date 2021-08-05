a14f05a522c809ae06d1162fa9a6aaa8c2f15dac
margoroma <margoroma2010@gmail.com>
2021-08-05 14:10:59 +0300

    feat: added functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function (array) {
+  if (arguments.length === 0) {
+      return 0;
+  } 
+  if (array.length > 0) {
+      return array.reduce((a, b) => a < b ? a : b);
+  } 
+  else {
+      return 0;
+  }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function (array) {
+  if (arguments.length === 0) {
+      return 0;
+  } 
+  if (array.length > 0) {
+      return array.reduce((a, b) => a > b ? a : b);
+  } 
+  else {
+      return 0;
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function (array) {
+  if (arguments.length === 0) {
+      return 0;
+  } 
+  if (array.length > 0) {
+      return array.reduce((a, b) => a + b) / array.length;
+  } 
+  else {
+      return 0;
+  }
+};
